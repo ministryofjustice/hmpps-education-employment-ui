5f89f0480dc20d47a073cfa887ba733fcbfdd98b
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2023-01-09 16:10:24 +0000

    ESWE-442_work_profile_page Added ids on all profile assets, started cypress

--- integration_tests/pages/workProfile.ts
+++ integration_tests/pages/workProfile.ts
@@ -1,7 +1,18 @@
 import Page from './page'
+import TrainingAndQualificationsPage from './trainingAndQualifications'
 
 export type PageElement = Cypress.Chainable<JQuery>
 
 export default class WorkProfilePage extends Page {
   changeStatusLink = (): PageElement => cy.get('[data-qa=change-status-link]')
+
+  tabOverview = (): PageElement => cy.get('[data-qa=tab-overview]')
+
+  tabDetails = (): PageElement => cy.get('[data-qa=tab-details]')
+
+  tabExperience = (): PageElement => cy.get('[data-qa=tab-experience]')
+
+  tabContacts = (): PageElement => cy.get('[data-qa=tab-contacts]')
+
+  tabTraining = (): PageElement => cy.get('[data-qa=tab-training]')
 }
