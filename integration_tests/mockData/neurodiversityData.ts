5db3bfd6ead510f75b877c910fe65990161c35b1
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2023-02-14 16:32:16 +0000

    Get_pom_client Added mock data for missing mock apis

---
+++ integration_tests/mockData/neurodiversityData.ts
@@ -0,0 +1,15 @@
+const data = [
+  {
+    prn: 'G6123VU',
+    establishmentId: 'MDI',
+    establishmentName: 'MOORLAND (HMP & YOI)',
+    neurodivergenceSelfDeclared: ['Dyslexia'],
+    selfDeclaredDate: '2022-02-18',
+    neurodivergenceAssessed: ['No Identified Neurodiversity Need'],
+    assessmentDate: '2022-05-18',
+    neurodivergenceSupport: ['Reading Support'],
+    supportDate: '2022-02-18',
+  },
+]
+
+export default data
