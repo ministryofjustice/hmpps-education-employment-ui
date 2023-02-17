9fa301d53aa7ad46275564ffc42e227ec6fc22ea
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2023-02-17 14:02:16 +0000

    ESWE-506_training_page_driving_license corrected value order

--- server/constants/contentLookup/trainingAndQualifications.ts
+++ server/constants/contentLookup/trainingAndQualifications.ts
@@ -1,7 +1,7 @@
 export default {
   ADVANCED_EDUCATION: 'Advanced school level qualification',
   CSCS: 'CSCS card',
-  DRIVING_LICENSE: 'UK driving licence',
+  DRIVING_LICENSE: 'Full UK driving licence',
   FIRST_AID: 'First aid certificate',
   FOOD_HYGIENE: 'Food hygiene certificate',
   HEALTH_AND_SAFETY: 'Health and safety',
