7a516a624ec9a48ba5b12178a13208374e493f09
mons kapinga <mons.kapinga@digital.justice.gov.uk>
2023-03-14 18:02:29 +0300

    ESWE-488_setup_google_analytics_ga4: generate a nonce for inline analytics script

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -1,5 +1,7 @@
-import express, { Router } from 'express'
+import crypto from 'crypto'
+import express, { Router, Request, Response } from 'express'
 import helmet from 'helmet'
+import config from '../config'
 
 export default function setUpWebSecurity(): Router {
   const router = express.Router()
@@ -7,19 +9,39 @@ export default function setUpWebSecurity(): Router {
   // Secure code best practice - see:
   // 1. https://expressjs.com/en/advanced/best-practice-security.html,
   // 2. https://www.npmjs.com/package/helmet
+  router.use((req, res, next) => {
+    res.locals.cspNonce = crypto.randomBytes(16).toString('hex')
+    next()
+  })
   router.use(
     helmet({
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ["'self'"],
+          formAction: ["'self'", new URL(config.apis.hmppsAuth.url).hostname],
           // Hash allows inline script pulled in from https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/template.njk
-          scriptSrc: ["'self'", 'code.jquery.com', "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='"],
-          styleSrc: ["'self'", 'code.jquery.com'],
+          // scriptSrc: ["'self'", 'code.jquery.com', "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='"],
+          scriptSrc: [
+            "'self'",
+            '*.google-analytics.com',
+            '*.googletagmanager.com',
+            (req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`,
+          ],
+          // styleSrc: ["'self'", 'code.jquery.com'],
+          styleSrc: ["'self'", (req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`],
           fontSrc: ["'self'"],
+          connectSrc: ['*.google-analytics.com', '*.googletagmanager.com', '*.analytics.google.com'],
+          imgSrc: ["'self'", '*.google-analytics.com', '*.googletagmanager.com'],
         },
       },
       crossOriginEmbedderPolicy: true,
     }),
   )
+
+  // cf. https://security-guidance.service.justice.gov.uk/implement-security-txt/
+  router.get('/.well-known/security.txt', (req, res) =>
+    res.redirect(301, 'https://security-guidance.service.justice.gov.uk/.well-known/security.txt'),
+  )
+
   return router
 }
