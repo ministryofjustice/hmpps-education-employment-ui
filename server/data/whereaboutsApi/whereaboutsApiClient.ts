3c87efcfc3287365ed48a15e2cd60d28e04777ee
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2023-04-13 16:39:16 +0100

    ESWE-538_tech_debt_unit_test_coverage add some more missing unit tests

--- server/data/whereaboutsApi/whereaboutsApiClient.ts
+++ server/data/whereaboutsApi/whereaboutsApiClient.ts
@@ -14,6 +14,7 @@ export default class WhereaboutsApiClient {
   async getUnacceptibleAbsenceCount(offenderNo: string, startDate: Date, endDate: Date) {
     const fromDate = startDate.toISOString().split('T')[0]
     const toDate = endDate.toISOString().split('T')[0]
+    console.log('restClientMock', this.restClient)
     const result = await this.restClient.get<GetUnacceptibleAbsenceCountResult>({
       path: `${BASE_URL}/offender/${offenderNo}/unacceptable-absence-count?fromDate=${fromDate}&toDate=${toDate}`,
     })
