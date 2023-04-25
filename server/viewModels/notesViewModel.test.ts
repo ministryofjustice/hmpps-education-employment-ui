27b04ec2a237a327a963e5888882b1a0a815a5c2
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2023-04-25 14:34:46 +0100

    ESWE-538_tech_debt_unit_test_coverage fixed failing circle ci test

--- server/viewModels/notesViewModel.test.ts
+++ server/viewModels/notesViewModel.test.ts
@@ -3,10 +3,11 @@ import { plainToClass } from 'class-transformer'
 import NotesViewModel from './notesViewModel'
 
 describe('NotesViewModel', () => {
+  const date = new Date('2022-04-30T13:00:00Z')
   const notesJson = {
     createdBy: 'Jane Doe',
     createdName: 'Jane',
-    createdDateTime: '2022-04-30T13:00:00Z',
+    createdDateTime: date.toLocaleString(),
     attribute: 'test',
     text: 'This is a note.',
   }
@@ -17,7 +18,7 @@ describe('NotesViewModel', () => {
     expect(notesViewModel.createdBy).toBe(notesJson.createdBy)
     expect(notesViewModel.createdName).toBe(notesJson.createdName)
     expect(notesViewModel.createdDateTime[0]).toEqual('30 Apr 2022')
-    expect(notesViewModel.createdDateTime[1]).toEqual('14:00')
+    expect(notesViewModel.createdDateTime[1]).toEqual(`${date.getHours()}:${date.getMinutes() || '00'}`)
     expect(notesViewModel.attribute).toBe(notesJson.attribute)
     expect(notesViewModel.text).toBe(notesJson.text)
   })
