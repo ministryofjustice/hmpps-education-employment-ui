24d8bf2f29c551a90f3381ab6f715329044fb79d
mons kapinga <mons.kapinga@digital.justice.gov.uk>
2023-04-25 13:28:25 +0100

    ESWE-576_get_appinsight_usage_data: set up custom dimension

--- server/utils/azureAppInsights.ts
+++ server/utils/azureAppInsights.ts
@@ -1,6 +1,11 @@
 import { config } from 'dotenv'
-import { setup, defaultClient, TelemetryClient, DistributedTracingModes } from 'applicationinsights'
+import { Contracts, setup, defaultClient, TelemetryClient, DistributedTracingModes } from 'applicationinsights'
+import { EnvelopeTelemetry } from 'applicationinsights/out/Declarations/Contracts'
 import applicationVersion from '../applicationVersion'
+
+export type ContextObject = {
+  [name: string]: any
+}
 
 function defaultName(): string {
   const {
@@ -16,7 +21,8 @@ function version(): string {
 
 export function initialiseAppInsights(): void {
   // Loads .env file contents into | process.env
-  config()
+  // config()
+  console.log(`initialiseAppInsights - instrumentation key = ${process.env.APPINSIGHTS_INSTRUMENTATIONKEY}`)
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
     // eslint-disable-next-line no-console
     console.log('Enabling azure application insights')
@@ -29,7 +35,25 @@ export function buildAppInsightsClient(name = defaultName()): TelemetryClient {
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
     defaultClient.context.tags['ai.cloud.role'] = name
     defaultClient.context.tags['ai.application.ver'] = version()
+    defaultClient.addTelemetryProcessor(addUserDataToRequests)
     return defaultClient
   }
   return null
+}
+
+export function addUserDataToRequests(envelope: EnvelopeTelemetry, contextObjects: ContextObject) {
+  const isRequest = envelope.data.baseType === Contracts.TelemetryTypeString.Request
+  if (isRequest) {
+    const { username, activeCaseLoadId } = contextObjects?.['http.ServerRequest']?.res?.locals?.user || {}
+    if (username) {
+      const { properties } = envelope.data.baseData
+      // eslint-disable-next-line no-param-reassign
+      envelope.data.baseData.properties = {
+        username,
+        activeCaseLoadId,
+        ...properties,
+      }
+    }
+  }
+  return true
 }
