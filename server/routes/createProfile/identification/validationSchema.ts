ca73c7886401639ade74696e377ab2bff6844532
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2023-07-28 11:46:42 +0100

    ESWE-662_bug_fixes Added missing validation (#114)

--- server/routes/createProfile/identification/validationSchema.ts
+++ server/routes/createProfile/identification/validationSchema.ts
@@ -13,24 +13,53 @@ export default function validationSchema(data: IdentificationData): ObjectSchema
   } = data
 
   const msg = `Select what type of ID ${firstName} ${lastName} has`
+  const msgOther = `Enter the type of ID ${firstName} ${lastName} has`
 
-  return joi.object({
-    identification: joi
-      .array()
-      .required()
-      .items(
-        joi
-          .any()
-          .valid(
-            IdentificationValue.BIRTH_CERTIFICATE,
-            IdentificationValue.DRIVING_LICENCE,
-            IdentificationValue.PASSPORT,
-            IdentificationValue.OTHER,
-          ),
-      )
-      .messages({
-        'any.only': msg,
-        'any.required': msg,
-      }),
-  })
+  return joi
+    .object({
+      identification: joi
+        .array()
+        .required()
+        .items(
+          joi
+            .any()
+            .valid(
+              IdentificationValue.BIRTH_CERTIFICATE,
+              IdentificationValue.DRIVING_LICENCE,
+              IdentificationValue.PASSPORT,
+              IdentificationValue.OTHER,
+            ),
+        )
+        .messages({
+          'any.only': msg,
+          'any.required': msg,
+        }),
+    })
+    .custom((obj, helper) => {
+      const { typeOfIdentificationDetails, identification } = obj
+
+      if (!identification.includes(IdentificationValue.OTHER)) {
+        return true
+      }
+
+      if (!typeOfIdentificationDetails) {
+        return helper.error('any.custom', {
+          key: 'typeOfIdentificationDetails',
+          label: 'typeOfIdentificationDetails',
+        })
+      }
+
+      if (typeOfIdentificationDetails.length > 200) {
+        return helper.error('any.length', {
+          key: 'typeOfIdentificationDetails',
+          label: 'typeOfIdentificationDetails',
+        })
+      }
+
+      return true
+    })
+    .messages({
+      'any.custom': msgOther,
+      'any.length': 'ID type must be 200 characters or less',
+    })
 }
