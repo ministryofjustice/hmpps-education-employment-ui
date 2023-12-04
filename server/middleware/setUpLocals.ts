7c4045e1feac4cc21b10d8f8085a7512286306c8
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2023-12-04 08:34:55 +0000

    update_latest_fallback_header_footer updated to latest versions of html (#140)

--- server/middleware/setUpLocals.ts
+++ server/middleware/setUpLocals.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import contentLookup from '../constants/contentLookup'
 import addressLookup from '../routes/addressLookup'
+import config from '../config'
 
 // Add constants and utilities to locals
 export default function setUpLocals(): Router {
@@ -10,6 +11,7 @@ export default function setUpLocals(): Router {
     res.locals.addressLookup = addressLookup
     res.locals.contentLookup = contentLookup
     res.locals.originalUrl = req.originalUrl
+    res.locals.manageDetailsLink = `${config.apis.hmppsAuth.externalUrl}/account-details`
 
     next()
   })
