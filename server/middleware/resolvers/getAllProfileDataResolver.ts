3b4946310b42f5c8b48f11c2522be0c00394b783
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2024-01-31 17:10:21 +0000

    Eswe 790 implement v2 fe library (#144)
    * ESWE-790_implement_v2_fe_library Updated code to latest version, fixed issues
    
    * ESWE-790_implement_v2_fe_library updated tag css
    
    * ESWE-790_implement_v2_fe_library Temp disabled failing API calls
    
    * ESWE-790_implement_v2_fe_library Fixed linting issues

--- server/middleware/resolvers/getAllProfileDataResolver.ts
+++ server/middleware/resolvers/getAllProfileDataResolver.ts
@@ -1,15 +1,12 @@
 import type { RequestHandler } from 'express'
 import { Services } from '../../services'
 
-import getCurrentOffenderActivities from './utils/getCurrentOffenderActivities'
 import getEmployabilitySkills from './utils/getEmployabilitySkills'
 import getLearnerEducation from './utils/getLearnerEducation'
 import getNeurodivergence from './utils/getNeurodivergence'
 import getLatestAssessment from './utils/getLatestAssessment'
 import getKeyworkerById from './utils/getKeyworkerById'
 import getUnacceptibleAbsenceCount from './utils/getUnacceptableAbsenceCount'
-import getComById from './utils/getComById'
-import getPomById from './utils/getPomById'
 
 // Gets profile data based on id parameter and puts it into request context
 const getAllProfileDataResolver =
@@ -17,51 +14,34 @@ const getAllProfileDataResolver =
   async (req, res, next): Promise<void> => {
     const { id } = req.params
     const { username } = res.locals.user
-    const {
-      prisonerSearchService,
-      keyworkerService,
-      prisonService,
-      curiousEsweService,
-      whereaboutsService,
-      deliusIntegrationService,
-      allocationManagerService,
-    } = services
+    const { prisonerSearchService, keyworkerService, curiousEsweService, whereaboutsService } = services
 
     try {
       const [
         prisoner,
-        currentOffenderActivities,
         employabilitySkills,
         learnerEducation,
         neurodivergence,
         learnerLatestAssessment,
         unacceptableAbsenceCount,
         keyworker,
-        com,
-        pom,
       ] = await Promise.all([
         prisonerSearchService.getPrisonerById(username, id),
-        getCurrentOffenderActivities(prisonService, username, id),
         getEmployabilitySkills(curiousEsweService, username, id),
         getLearnerEducation(curiousEsweService, username, id),
         getNeurodivergence(curiousEsweService, username, id),
         getLatestAssessment(curiousEsweService, username, id),
         getUnacceptibleAbsenceCount(whereaboutsService, username, id),
         getKeyworkerById(keyworkerService, username, id),
-        getComById(deliusIntegrationService, username, id),
-        getPomById(allocationManagerService, username, id),
       ])
 
       req.context.prisoner = prisoner
-      req.context.currentOffenderActivities = currentOffenderActivities
       req.context.employabilitySkills = employabilitySkills
       req.context.learnerEducation = learnerEducation
       req.context.learnerLatestAssessment = learnerLatestAssessment
       req.context.neurodivergence = neurodivergence
       req.context.unacceptableAbsenceCount = unacceptableAbsenceCount
       req.context.keyworker = keyworker
-      req.context.com = com
-      req.context.pom = pom
 
       next()
     } catch (err) {
