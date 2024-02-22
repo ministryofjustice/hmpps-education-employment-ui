7c97ff7cac943d6ed4cf65ddc05fe6c4bdb41565
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2024-02-22 12:54:01 +0000

    ESWE-791 prisoner view cms (#146)
    * ESWE-791_prisoner_view_cms basic design and supporting code
    
    * ESWE-791_prisoner_view_cms Completed filters HTML
    
    * ESWE-791_prisoner_view_cms Minor refactor, tidy
    
    * ESWE-791_prisoner_view_cms Added new service, developed filter handling
    
    * ESWE-791_prisoner_view_cms Added no results options
    
    * ESWE-791_prisoner_view_cms added validation
    
    * ESWE-791_prisoner_view_cms Added validation
    
    * ESWE-791_prisoner_view_cms Added unit tests
    
    * ESWE-791_prisoner_view_cms Added resolver unit tests
    
    * ESWE-791_prisoner_view_cms Fixed failing tests

---
+++ server/middleware/resolvers/getPrisonerListMatchJobsResolver.ts
@@ -0,0 +1,49 @@
+import type { RequestHandler } from 'express'
+
+import config from '../../config'
+import { formatDateToyyyyMMdd, offenderEarliestReleaseDate } from '../../utils/utils'
+import PrisonerProfileService from '../../services/prisonerProfileService'
+
+// Gets prisoner based on id parameter and puts it into request context
+const getPrisonerListMatchJobsResolver =
+  (prisonerProfileService: PrisonerProfileService): RequestHandler =>
+  async (req, res, next): Promise<void> => {
+    const { page, sort, order, showNeedsSupportFilter, prisonerNameFilter = '', typeOfWorkFilter = '' } = req.query
+    const { userActiveCaseLoad } = res.locals
+    const { username, token } = res.locals.user
+
+    // Prepare search & date parameters
+    const { weeksBeforeRelease } = config
+
+    // Build search filter
+    const searchFilter = {
+      status: showNeedsSupportFilter ? ['READY_TO_WORK', 'SUPPORT_NEEDED'] : ['READY_TO_WORK'],
+      typeOfWork: decodeURIComponent(typeOfWorkFilter.toString()),
+      searchTerm: decodeURIComponent(prisonerNameFilter.toString()),
+    }
+
+    // Build date filter
+    const dateFilter = {
+      earliestReleaseDate: formatDateToyyyyMMdd(new Date().toString()),
+      latestReleaseDate: offenderEarliestReleaseDate(weeksBeforeRelease),
+      prisonIds: [userActiveCaseLoad.caseLoadId],
+    }
+
+    try {
+      req.context.prisonerListMatchedJobs = await prisonerProfileService.getPrisonersToMatchJobs({
+        userToken: token,
+        username,
+        dateFilter,
+        searchFilter,
+        sort,
+        order,
+        page: page ? +page - 1 : 0,
+      })
+
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }
+
+export default getPrisonerListMatchJobsResolver
