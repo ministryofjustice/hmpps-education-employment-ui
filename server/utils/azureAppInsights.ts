8f50bdaab8b7fb4e137aa7b59aadcf33a72c753e
mons <mons.kapinga@digital.justice.gov.uk>
2024-03-06 11:34:52 +0000

    update dependencies and update appinsights deprecated key (#148)

--- server/utils/azureAppInsights.ts
+++ server/utils/azureAppInsights.ts
@@ -24,7 +24,7 @@ export function initialiseAppInsights(): void {
   // Loads .env file contents into | process.env
   config()
 
-  if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+  if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
     // eslint-disable-next-line no-console
     console.log('Enabling azure application insights')
 
@@ -33,7 +33,7 @@ export function initialiseAppInsights(): void {
 }
 
 export function buildAppInsightsClient(name = defaultName()): TelemetryClient {
-  if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+  if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
     defaultClient.context.tags['ai.cloud.role'] = name
     defaultClient.context.tags['ai.application.ver'] = version()
     defaultClient.addTelemetryProcessor(addUserDataToRequests)
