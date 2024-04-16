7e24045587c4c83db6d66db43211125c0de0b848
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2024-04-16 15:34:54 +0100

    Eswe 813 matched jobs page (#161)
    * ESWE-813_matched_jobs_page Added page framework
    
    * ESWE-813_matched_jobs_page Started HTML, and data binding
    
    * ESWE-813_matched_jobs_page Finished data binding, added filters
    
    * ESWE-813_matched_jobs_page wired up sortis and filters, added mocks
    
    * ESWE-813_matched_jobs_page Added fiter reset, back, no results
    
    * ESWE-813_matched_jobs_page Tidy, completed unit tests

--- server/middleware/resolvers/utils/getFlaggedJobs.ts
+++ server/middleware/resolvers/utils/getFlaggedJobs.ts
@@ -1,9 +1,21 @@
 import JobService from '../../../services/jobService'
 
-const getFlaggedJobs = async (jobService: JobService, username: string, id: string) => {
+const getFlaggedJobs = async (
+  jobService: JobService,
+  username: string,
+  params: {
+    offenderNo: string
+    page?: number
+    sort?: string
+    order?: string
+    typeOfWorkFilter?: string
+    locationFilter?: string
+    distanceFilter?: string
+  },
+) => {
   try {
     // Get Flagged jobs
-    return await jobService.getFlaggedJobs(username, id)
+    return await jobService.getFlaggedJobs(username, params)
   } catch (err) {
     // Handle no data
     if (err?.status === 404) {
--- server/routes/candidateMatching/index.ts
+++ server/routes/candidateMatching/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import type { Services } from '../../services'
 import prisonerListMatchJobsRoutes from './prisonerListMatchJobs'
+import matchedJobs from './matchedJobs'
 
 export default function attachRoutes(router: Router, services: Services): void {
   prisonerListMatchJobsRoutes(router, services)
+  matchedJobs(router, services)
 }
