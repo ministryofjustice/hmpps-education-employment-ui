1d5c71e4dfa2134b703a0c68f754fba941f39a91
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2024-04-19 15:59:27 +0100

    ESWE-814_flagged_jobs_page Added html

--- server/routes/candidateMatching/index.ts
+++ server/routes/candidateMatching/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import type { Services } from '../../services'
 import prisonerListMatchJobsRoutes from './prisonerListMatchJobs'
-import matchedJobs from './matchedJobs'
+import matchedJobsRoutes from './matchedJobs'
+import flaggedJobsRoutes from './flaggedJobs'
 
 export default function attachRoutes(router: Router, services: Services): void {
   prisonerListMatchJobsRoutes(router, services)
-  matchedJobs(router, services)
+  matchedJobsRoutes(router, services)
+  flaggedJobsRoutes(router, services)
 }
