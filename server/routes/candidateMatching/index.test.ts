fbc064e55b1c94c25532759cbdc110d3812f84cf
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2024-04-24 12:45:11 +0100

    Eswe 814 flagged jobs page (#164)
    * ESWE-814_flagged_jobs_page Added controller and resolver
    
    * ESWE-814_flagged_jobs_page Added html
    
    * ESWE-814_flagged_jobs_page Tidy, missing tests, default sorts

--- server/routes/candidateMatching/index.test.ts
+++ server/routes/candidateMatching/index.test.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Router } from 'express'
 import prisonerListMatchJobs from './prisonerListMatchJobs'
+import matchedJobsRoutes from './matchedJobs'
+import flaggedJobsRoutes from './flaggedJobs'
 import routes from '.'
 import { Services } from '../../services'
 
 jest.mock('./prisonerListMatchJobs')
+jest.mock('./matchedJobs')
+jest.mock('./flaggedJobs')
 jest.mock('express', () => ({
   Router: jest.fn().mockImplementation(() => ({
     use: jest.fn(),
@@ -28,5 +32,7 @@ describe('Server routes', () => {
   it('calls editActionRoutes with router and services', () => {
     routes(router as any, services as any)
     expect(prisonerListMatchJobs).toHaveBeenCalledWith(router, services)
+    expect(matchedJobsRoutes).toHaveBeenCalledWith(router, services)
+    expect(flaggedJobsRoutes).toHaveBeenCalledWith(router, services)
   })
 })
