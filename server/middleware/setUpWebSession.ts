830561e23b69deff0d64c576bdb9d6f3ca8ed7ea
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-05-30 09:35:46 +0100

    Fix redis client

--- server/middleware/setUpWebSession.ts
+++ server/middleware/setUpWebSession.ts
@@ -9,7 +9,7 @@ import logger from '../../logger'
 const RedisStore = connectRedis(session)
 
 export default function setUpWebSession(): Router {
-  const client = createRedisClient({ legacyMode: true })
+  const client = createRedisClient()
   client.connect().catch((err: Error) => logger.error(`Error connecting to Redis`, err))
 
   const router = express.Router()
