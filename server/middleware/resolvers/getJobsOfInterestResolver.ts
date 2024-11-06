c7566c16f717d6144cfdbc4e309299467cbafb25
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2024-11-06 15:49:51 +0000

    ESWE-1000_implement_interested_jobs implemented new jobs of interest end point

--- server/middleware/resolvers/getJobsOfInterestResolver.ts
+++ server/middleware/resolvers/getJobsOfInterestResolver.ts
@@ -2,20 +2,31 @@ import type { RequestHandler } from 'express'
 
 import JobService from '../../services/jobService'
 import getJobsOfInterest from './utils/getJobsOfInterest'
+import { getSessionData, setSessionData } from '../../utils/session'
+import getPrisonerAddressById from './utils/getPrisonerAddressById'
+import DeliusIntegrationService from '../../services/deliusIntegrationService'
 
 const getJobsOfInterestResolver =
-  (jobService: JobService): RequestHandler =>
+  (jobService: JobService, deliusIntegrationService: DeliusIntegrationService): RequestHandler =>
   async (req, res, next): Promise<void> => {
     const { id } = req.params
     const { username } = res.locals.user
-    const { page, sort = '', order = '' } = req.query
+    const { page = '1', sort = '', order = '' } = req.query
 
     try {
+      if (getSessionData(req, ['prisonerAddress', id])) {
+        req.context.prisonerAddress = getSessionData(req, ['prisonerAddress', id])
+      } else {
+        req.context.prisonerAddress = await getPrisonerAddressById(deliusIntegrationService, username, id)
+        setSessionData(req, ['prisonerAddress', id], req.context.prisonerAddress)
+      }
+
       const jobsOfInterest = await getJobsOfInterest(jobService, username, {
         offenderNo: id,
         page: Number(page),
         sort: sort.toString(),
         order: order.toString(),
+        locationFilter: req.context.prisonerAddress ? req.context.prisonerAddress.postcode : '',
       })
 
       req.context.jobsOfInterestResults = jobsOfInterest
