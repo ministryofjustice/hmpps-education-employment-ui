68e7374360b058f2ed39e2bad0d675dec272b132
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2024-12-16 15:59:43 +0000

    ESWE-1172_csp_and_in_line_style_fixes Added csp haddening, removed inline styles (#227)

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -1,5 +1,5 @@
 import crypto from 'crypto'
-import express, { Router, Request, Response } from 'express'
+import express, { Router, Request, Response, NextFunction } from 'express'
 import helmet from 'helmet'
 import config from '../config'
 
@@ -9,41 +9,39 @@ export default function setUpWebSecurity(): Router {
   // Secure code best practice - see:
   // 1. https://expressjs.com/en/advanced/best-practice-security.html,
   // 2. https://www.npmjs.com/package/helmet
-  router.use((req, res, next) => {
+  router.use((_req: Request, res: Response, next: NextFunction) => {
     res.locals.cspNonce = crypto.randomBytes(16).toString('hex')
     next()
   })
-
-  const scriptSrc = [
-    "'self'",
-    '*.google-analytics.com',
-    '*.googletagmanager.com',
-    (req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`,
-  ]
-  const styleSrc = ["'self'", 'code.jquery.com', "'unsafe-inline'"]
-  const imgSrc = ["'self'", 'data:']
-  const fontSrc = ["'self'"]
-  const connectSrc = ['*.google-analytics.com', '*.googletagmanager.com', '*.analytics.google.com']
-
-  if (config.apis.frontendComponents.url) {
-    scriptSrc.push(config.apis.frontendComponents.url)
-    styleSrc.push(config.apis.frontendComponents.url)
-    imgSrc.push(config.apis.frontendComponents.url)
-    fontSrc.push(config.apis.frontendComponents.url)
-  }
-
   router.use(
     helmet({
       contentSecurityPolicy: {
         directives: {
-          defaultSrc: ["'self'"],
-          formAction: ["'self'", new URL(config.apis.hmppsAuth.url).hostname],
-          // Hash allows inline script pulled in from https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/template.njk
-          scriptSrc,
-          styleSrc,
-          fontSrc,
-          imgSrc,
-          connectSrc,
+          defaultSrc: ["'self'", config.apis.frontendComponents.url],
+          // This nonce allows us to use scripts with the use of the `cspNonce` local, e.g (in a Nunjucks template):
+          // <script nonce="{{ cspNonce }}">
+          // or
+          // <link href="http://example.com/" rel="stylesheet" nonce="{{ cspNonce }}">
+          // This ensures only scripts we trust are loaded, and not anything injected into the
+          // page by an attacker.
+          scriptSrc: [
+            "'self'",
+            'code.jquery.com',
+            '*.google-analytics.com',
+            '*.googletagmanager.com',
+            config.apis.frontendComponents.url,
+            (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`,
+          ],
+          styleSrc: [
+            "'self'",
+            'code.jquery.com',
+            config.apis.frontendComponents.url,
+            (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`,
+          ],
+          fontSrc: ["'self'", config.apis.frontendComponents.url],
+          formAction: [`'self' ${config.apis.hmppsAuth.externalUrl}`],
+          connectSrc: ['*.google-analytics.com', '*.googletagmanager.com', '*.analytics.google.com'],
+          objectSrc: ["'none'"], // Disallow <object> or <embed> tags
         },
       },
       crossOriginEmbedderPolicy: true,
