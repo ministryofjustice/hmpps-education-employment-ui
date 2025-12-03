9ca6d6deb13b784ec9cc9a420566c27503512efa
Mary Ford <mary.ford@justice.gov.uk>
2025-12-03 16:40:24 +0000

    ESWE-1445: Use new keyworker API endpoint (#285)
    Switch from using `GET /key-worker/offender/{offenderNo}` to `GET
    /prisoners/{personIdentifier}/allocations/current`.
    
    Note: **Before** deploying to preprod/prod, the
    ROLE_ALLOCATIONS__ALLOCATIONS__RO role needs to be added to the
    following clients:
    Preprod: hmpps-education-employment-ui-client-1
    Prod: hmpps-education-employment-ui-client-2
    
    This can be requested as per
    https://mojdt.slack.com/archives/C02S71KUBED/p1758718289005399.
    
    ---------
    
    Co-authored-by: zahid.khan <Zahid.Khan1@justice.gov.uk>

--- server/services/keyworkerService.ts
+++ server/services/keyworkerService.ts
@@ -1,6 +1,6 @@
 import HmppsAuthClient from '../data/hmppsAuthClient'
 import KeyworkerApiClient from '../data/keyworkerApi/keyworkerApiClient'
-import NomisUserRolesApiClient from '../data/nomisUserRolesApi/nomisUserRolesApiClient'
+import convertToTitleCase from '../utils/convertToTitleCase'
 
 export default class KeyworkerService {
   constructor(private readonly hmppsAuthClient: HmppsAuthClient) {}
@@ -8,16 +8,29 @@ export default class KeyworkerService {
   async getKeyworkerForOffender(
     username: string,
     id: string,
-  ): Promise<{ firstName: string; lastName: string; email: string }> {
+  ): Promise<{ firstName?: string; lastName?: string; email?: string; success: boolean }> {
     const systemToken = await this.hmppsAuthClient.getSystemClientToken(username)
 
-    const keyworkerResult = await new KeyworkerApiClient(systemToken).getKeyworkerForOffender(id)
-    const staffDetails = await new NomisUserRolesApiClient(systemToken).getStaffDetails(keyworkerResult.staffId)
+    try {
+      const staffAllocationsResult = await new KeyworkerApiClient(systemToken).getStaffAllocationsForOffender(id)
+      const keyWorkerAllocation = staffAllocationsResult?.allocations.find(a => a.policy.code === 'KEY_WORKER')
 
-    return {
-      firstName: staffDetails.firstName,
-      lastName: staffDetails.lastName,
-      email: staffDetails.primaryEmail,
+      // Handle case where no key worker is allocated
+      const keyWorkerFirstName = keyWorkerAllocation?.staffMember?.firstName
+      const keyWorkerLastName = keyWorkerAllocation?.staffMember?.lastName
+      if (!keyWorkerFirstName || !keyWorkerLastName) {
+        return undefined
+      }
+      return {
+        firstName: convertToTitleCase(keyWorkerFirstName),
+        lastName: convertToTitleCase(keyWorkerLastName),
+        email: keyWorkerAllocation?.staffMember?.emailAddresses[0],
+        success: true,
+      }
+    } catch (error) {
+      return {
+        success: false,
+      }
     }
   }
 }
